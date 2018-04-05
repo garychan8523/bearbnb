c9e192016f4af3734d4a1bf0ceb6ce8c263d000f
garychan8523 <garychan8523@gmail.com>
2018-04-05 16:13:41 +0800

    add dev config

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: "913110617461-13ud0uf2a865tm30o276bgpb60emaar0.apps.googleusercontent.com",
+  googleClientSecret: "0HHyqHvQ04yQ0tG0Y9VOHUXG",
+  mongoURI: "mongodb://127.0.0.1/bears",
+  cookieKey: "AIzaSyA8VuVV8IJjT_2K-qyblYXXvh9wA5HlwHU"
+};
